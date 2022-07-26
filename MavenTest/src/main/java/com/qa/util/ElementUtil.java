5877c64c41e6ff405cbdb8c21a42bffeb82dd8ad
prasantaess <prasanta.ess@gmail.com>
2022-07-26 17:21:54 +0530

    Add Utill

--- MavenTest/src/main/java/com/qa/util/ElementUtil.java
+++ MavenTest/src/main/java/com/qa/util/ElementUtil.java
@@ -59,5 +59,88 @@ public class ElementUtil {
 		System.out.println("Searching for the Elemnet Started at"+ java.time.LocalTime.now());
 				
 	}
+	
+	public void flash(WebElement element) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		String bgcolor = element.getCssValue("backgroundColor");
+		for (int i = 0; i < 15; i++) {
+			changeColor("rgb(0,200,0)", element);// 1
+			changeColor(bgcolor, element);// 2
+		}
+	}
+
+	private void changeColor(String color, WebElement element) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].style.backgroundColor = '" + color + "'", element);
+
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	public String getTitleByJS() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		return js.executeScript("return document.title;").toString();
+	}
+
+	public void refreshBrowserByJS() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("history.go(0)");
+	}
+
+	public void generateAlert(String message) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("alert('" + message + "')");
+	}
+
+	public String getPageInnerText() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		return js.executeScript("return document.documentElement.innerText;").toString();
+	}
+
+	public void clickElementByJS(WebElement element) {
+		 WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		 wait.until(ExpectedConditions.elementToBeClickable(element));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", element);
+	}
+
+	public void sendKeysUsingWithId(String id, String value) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("document.getElementById('" + id + "').value='" + value + "'");
+	}
+	public void javaScriptSendKeys(WebElement ele, String value) {
+		 ele.clear();
+	//	 ele.sendKeys(value);
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		String val = "arguments[0].value='" + value + "';";
+		js.executeScript(val, ele);
+	}
+
+	public void scrollPageDown() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+	}
+
+	public void scrollPageDown(String height) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, '" + height + "')");
+	}
+
+	public void scrollPageUp() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(document.body.scrollHeight, 0)");
+	}
+
+	public void scrollIntoView(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true);", element);
+	}
+
+	public void drawBorder(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].style.border='3px solid red'", element);
+	}
 
 }
